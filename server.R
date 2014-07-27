39e5294cf4d877ca2544a739daf6a02e4b7a50c3
Nguyen Thanh Trung <ngthanhtrung23@gmail.com>
2014-07-27 22:41:17 +0800

    Update UI

--- server.R
+++ server.R
@@ -5,24 +5,41 @@ library(caret)
 library(RCurl)
 
 # Load the Titanic dataset
-url <- getURL('https://raw.githubusercontent.com/R-R-/CourseraDevelopingDataProduct/master/train.csv')
-Titanic = read.csv(text = url)
-goodColumnsNoLabel <- c("Pclass", "Sex", "Age")
+if (file.exists('train.csv')) { # The file is in local
+    Titanic = read.csv('train.csv')
+} else {
+    url <- getURL('https://raw.githubusercontent.com/R-R-/CourseraDevelopingDataProduct/master/train.csv')
+    Titanic = read.csv(text = url)
+}
+goodColumnsNoLabel <- c("Pclass", "Sex", "Age", "Parch")
 goodColumns <- c("Survived", goodColumnsNoLabel)
 Titanic <- Titanic[, goodColumns]
 Titanic$Pclass <- factor(Titanic$Pclass)
 Titanic$Survived <- factor(Titanic$Survived)
+Titanic$Parch <- factor(Titanic$Parch)
 
 # Create a prediction model with caret
 modelFit <- train(Survived ~ ., data = Titanic, method = "glm")
 
 # Our predict function
-makePredict <- function (age, type, sex) {
-    prediction <- predict(modelFit, data.frame(Age=age, Pclass=type, Sex=sex))
-    if (prediction == 0) {
-        "Not survive"
+makePredict <- function (age, type, sex, parch) {
+    prediction <- predict(modelFit, data.frame(Age=age, Pclass=type, Sex=sex, Parch=parch))
+    
+    dice <- runif(1, 0, 1.0)
+    if (dice < 0.25) {
+        prefix <- "We predict that you will"
+    } else if (dice < 0.50) {
+        prefix <- "You will likely"
+    } else if (dice < 0.75) {
+        prefix <- "There is a high chance that you will"
     } else {
-        "Survive"
+        prefix <- "Our data suggest that you will"
+    }
+    
+    if (prediction == 0) {
+        paste(prefix, "not survive :(")
+    } else {
+        paste(prefix, "survive")
     }
 }
 
@@ -32,10 +49,13 @@ shinyServer(
         output$age <- renderPrint({input$age})
         output$type <- renderPrint({input$type})
         output$sex <- renderPrint({input$sex})
+        output$family <- renderPrint({input$parch})
         
         output$survive <- renderText({
             if (input$goButton > 0) {
-                isolate(makePredict(input$age, input$type, input$sex))
+                isolate(makePredict(input$age, input$type, input$sex, input$parch))
+            } else {
+                "Please enter your information and click Submit"
             }
         })
     }
--- ui.R
+++ ui.R
@@ -1,22 +1,57 @@
 library(shiny)
 
-shinyUI(pageWithSidebar(
-    headerPanel("Can you survive the Titanic?"),
-    sidebarPanel(
-        numericInput('age', 'Your age:', 20, min = 1, max = 100, step = 1),
-        radioButtons('type', 'Your class: ', c(
-            '1st' = '1',
-            '2nd' = '2',
-            '3rd' = '3'
+shinyUI(fluidPage(
+    titlePanel("Can you survive the Titanic?"),
+    
+    fluidRow(
+        column(6, wellPanel(
+            numericInput('age', 'Your age:', 20, min = 1, max = 100, step = 1),
+            radioButtons('type', 'What ticket would you buy: ', c(
+                '1st class' = '1',
+                '2nd class' = '2',
+                '3rd class' = '3'
+            )),
+            radioButtons('sex', 'Your sex: ', c(
+                'Male' = 'male',
+                'Female' = 'female'
+            )),
+            radioButtons('parch', 'Would you bring your family onboard? If yes, how many family members?', c(
+                'No' = '0',
+                '1' = '1',
+                '2' = '2',
+                '3' = '3',
+                '4' = '4',
+                '5' = '5',
+                '6' = '6'
+            )),
+            actionButton('goButton', 'Submit')
         )),
-        radioButtons('sex', 'Your sex: ', c(
-            'Male' = 'male',
-            'Female' = 'female'
-        )),
-        actionButton('goButton', 'Submit')
+        column(6, wellPanel(
+            h3('Prediction result (you need to click Submit for this to update)'),
+            verbatimTextOutput('survive'),
+            h3('Your data'),
+            h5('Your age:'),
+            verbatimTextOutput('age'),
+            h5('Your ticket:'),
+            verbatimTextOutput('type'),
+            h5('Your sex:'),
+            verbatimTextOutput('sex'),
+            h5('Family members onboard:'),
+            verbatimTextOutput('family')
+        ))
     ),
-    mainPanel(
-        h3('We predict that you will'),
-        verbatimTextOutput('survive')
+    fluidRow(
+        h3('About this app'),
+        h4('How this app works'),
+        p('Using the Titanic survival data available at https://www.kaggle.com/c/titanic-gettingStarted/data, 
+            we were able to build a prediction model was built. Based on your age, ticket class, gender and 
+            number of family members onboard, we can predict your chance of surviving the Titanic shipwreck.'
+        ),
+        h4('How to use this app'),
+        p('Using this application is simple. You just need to fill in all the information and click Submit.
+          After Submit is clicked, your data will be send to our server. Our prediction model will then be
+          used to predict whether you can survive the Titanic shipwreck.
+          Note: As soon as you select new choices, the "Your data" section on the right will immediately
+          change. However, you will need to click on Submit for us to generate new prediction.')
     )
 ))
